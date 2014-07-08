172bbc84332317a6d0ada0f183719e06f5863ad4
fullhdpixel <tsmudde@hotmail.com>
2014-07-08 17:18:37 +0200

    Content added

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 				'verticalCentered': true,
 				'css3': true,
 				'sectionsColor': ['#F0F2F4', '#fff', '#fff', '#fff'],
-				'navigation': true,
+				'navigation': false,
 				'easing': 'easeInQuart',
 				'sectionsColor': ['white', 'black', 'white', 'black', '#white'],
 				'navigationPosition': 'left',
@@ -75,37 +75,35 @@
 		});
 	</script>
 </head>
-<body>
+<body data-spy="scroll" data-offset="0" data-target="#myScrollspy">
 
 <nav class="navbar-default navbar-static-top navbar-trans" role="navigation">
-	 <div class="container-fluid">
+	<div class="container-fluid">
 		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<button type="button" class="navbar-toggle" data-toggle="collapse">
 			<span class="sr-only">Toggle navigation</span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
-		  </button>
-		</div>
+		</button>
+	</div>
 
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav nav-justified">
-				<li id="active"><a href="#slide1">Home</a></li>
-				<li><a href="#slide2">Slide 2</a></li>
-				<li><a href="#slide3">Slide 3</a></li>
-				<li><a href="#slide4">Slide 4</a></li>
-				<li><a href="#slide5">Slide 5</a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Exchanges<b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li>Page1</li>
-						<li>Page2</li>
-						<li>Page3</li>
-						<li>Page4</li>
-					</ul>
-				</li>
-			</ul>
-		</div>
+	<div class="collapse navbar-collapse" id="myScrollspy">
+		<ul class="nav navbar-nav nav-justified">
+			<li id="active"><a href="#about">About</a></li>
+			<li><a href="#features">Features</a></li>
+			<li><a href="#downloads">Downloads</a></li>
+			<li><a href="#exchanges">Exchanges</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Exchanges<b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="https://bittrex.com/Market/?MarketName=BTC-VOOT" target="_blank">Bittrex</a></li>
+					<li><a href="" target="_blank">Bluetrade</a></li>
+					<li><a href="" target="_blank">Coins-e</a></li>
+				</ul>
+			</li>
+		</ul>
 	</div>
 </nav>	
 
@@ -115,72 +113,106 @@
 </div>
 
 <div id="content">
-	<div id="staticImg">
-		<div class="imgsContainer">
-			<img src="http://placehold.it/350x670" alt="iphone" id="iphone-green" />
-		</div>
-	</div>
-
-	<div class="section" id="section0">
+	<section class="section" id="about">
 	    <div class="slide" id="slide1" data-anchor="slide1">
 			<div class="intro">
+				<img src="./images/1.jpg" />
 				<h1>Home</h1>
 				<p>
 					Main slide.
 				</p>
 			</div>
 		</div>
-
 	    <div class="slide" id="slide2" data-anchor="slide2">
+			<img src="./images/2.jpg" />
 			<h1>Slide 2</h1>
 		</div>
 		<div class="slide" id="slide3" data-anchor="slide3">
+			<img src="./images/3.jpg" />
 			<h1>Slide 3</h1>
 		</div>
 		<div class="slide" id="slide4" data-anchor="slide4">
+			<img src="./images/4.jpg" />
 			<h1>Slide 4</h1>
 		</div>
-		<div class="slide" id="slide5" data-anchor="slide5">
-			<h1>Slide 5</h1>
-		</div>
-	</div>
-	<div class="section" id="section1">
-		<div class="wrap">
-			<div class="imgsContainer">
-				<img src="http://placehold.it/350x670" alt="iphone" id="iphone2" />
-				<img src="http://placehold.it/350x670" alt="iphone" id="iphone3" />
-				<img src="http://placehold.it/350x670" alt="iphone" id="iphone4" />
-			</div>
-
-			<div class="box">
-				<h2></h2>
+	</section>
+	<section class="section" id="features">
+		<div class="container">
+			<div class="row">
+				<h2>Features</h2>
+				<h3><small>Everything a VootCoin user needs to meet his/her core security objectives more effectively.</small></h3>
 			</div>
 		</div>
-	</div>
+		<div class="slide" id="slide1" data-anchor="slide1">
+			<div class="">
+				<div class="intro">
+					<h2>Decentralized Anonymity</h2>
+					<p>
+						Crypto-currencies are based on a blockchain and non-anonymity; transactions are always signed by the coin owner and thus the transaction chain includes information of the coin origins. There have been suggestions to hide this information
+						and one already supported but not implemented is CoinJoin based transaction grouping. Nitesend is a decentralized implementation of this proposal.
+					</p>
+				</div>
+			</div>
 
-	<div class="section moveDown" id="section2">
+			<div class="slide" id="slide2" data-anchor="slide2">
+				<h1>Slide 2</h1>
+				<!--
+				<div class="imgsContainer">
+					<img src="http://placehold.it/350x670" alt="iphone" id="iphone2" />
+					<img src="http://placehold.it/350x670" alt="iphone" id="iphone3" />
+					<img src="http://placehold.it/350x670" alt="iphone" id="iphone4" />
+				</div>
+				-->
+			</div>
+			<div class="slide" id="slide3" data-anchor="slide3">
+				<h1>Slide 3</h1>
+			</div>
+			<div class="slide" id="slide4" data-anchor="slide4">
+				<h1>Slide 4</h1>
+			</div>
+			<div class="slide" id="slide5" data-anchor="slide5">
+				<h1>Slide 5</h1>
+			</div>
+		</div>
+	</section>
+	<section class="section moveDown" id="downloads">
 		<div class="wrap">
+			<!--
 			<div class="imgsContainer">
 				<img src="http://placehold.it/350x670" alt="iphone" id="iphone-yellow" />
 				<img src="http://placehold.it/350x670" alt="iphone" id="iphone-red" />
 				<img src="http://placehold.it/670x350" alt="iphone" id="iphone-blue" />
 			</div>
-
+			-->
 			<div class="box">
 				<h2></h2>
 			</div>
 		</div>
-	</div>
-	<div class="section moveDown" id="section3">
+	</section>
+	<section class="section moveDown" id="exchanges">
 		<div class="wrap">
 			<div class="box">
 				<h2></h2>
 			</div>
 		</div>
+		<!--
 		<div class="imgsContainer">
 			<img src="http://placehold.it/350x670" alt="iphone" id="iphone-two" />
 		</div>
-	</div>
+		-->
+	</section>
+	<section class="section" id="contact">
+		<div class="wrap">
+			<div class="box">
+				<h2></h2>
+			</div>
+		</div>
+		<!--
+		<div class="imgsContainer">
+			<img src="http://placehold.it/350x670" alt="iphone" id="iphone-two" />
+		</div>
+		-->
+	</section>
 </div>
 
 <div id="footer">
